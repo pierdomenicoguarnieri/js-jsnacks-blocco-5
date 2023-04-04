f67c11485e30ed61ea7bb6e57ca249794c2b3f13
pierdomenicoguarnieri <pierdo.guarnieri@gmail.com>
2023-04-04 16:10:44 +0200

    add: array of objects

---
+++ jsnacks-1/assets/js/script.js
@@ -0,0 +1,52 @@
+const users =[
+  {
+    name: "Teresa",
+    surname:"Taranto",
+    age: 63
+  },
+  {
+    name: "Hugh",
+    surname:"Moser",
+    age: 65
+  },
+  {
+    name: "Ida",
+    surname:"Giordano",
+    age: 7
+  },
+  {
+    name: "Nina",
+    surname:"Manfrin",
+    age: 55
+  },
+  {
+    name: "Quinto",
+    surname:"Davide",
+    age: 17
+  },
+  {
+    name: "Fedro",
+    surname:"Palermo",
+    age: 33
+  },
+  {
+    name: "Ada",
+    surname:"Calabrese",
+    age: 18
+  },
+  {
+    name: "Remo",
+    surname:"Calabresi",
+    age: 78
+  },
+  {
+    name: "Alessabdro",
+    surname:"Sagese",
+    age: 77
+  },
+  {
+    name: "Bianca",
+    surname:"Romano",
+    age: 22
+  }
+];
