afb67d1918e5ff85e1b6751573a83e5ca9de3868
pierdomenicoguarnieri <pierdo.guarnieri@gmail.com>
2023-04-05 19:42:05 +0200

    add: array of elements

---
+++ jsnacks-6/assets/script.js
@@ -0,0 +1,43 @@
+const squadre = [
+  {
+    nome: "Roma",
+    puntiFatti: 0,
+    falliSubiti: 0,
+  },
+  {
+    nome: "Inter",
+    puntiFatti: 0,
+    falliSubiti: 0,
+  },
+  {
+    nome: "Juve",
+    puntiFatti: 0,
+    falliSubiti: 0,
+  },
+  {
+    nome: "Milan",
+    puntiFatti: 0,
+    falliSubiti: 0,
+  },
+  {
+    nome: "Torino",
+    puntiFatti: 0,
+    falliSubiti: 0,
+  },
+  {
+    nome: "Napoli",
+    puntiFatti: 0,
+    falliSubiti: 0,
+  },
+  {
+    nome: "Atalanta",
+    puntiFatti: 0,
+    falliSubiti: 0,
+  },
+  {
+    nome: "Hellas Verona",
+    puntiFatti: 0,
+    falliSubiti: 0,
+  },
+];
+
