2b5c5b227807c8068d956c78007bc418338cb09a
pierdomenicoguarnieri <pierdo.guarnieri@gmail.com>
2023-04-05 19:21:48 +0200

    add: random weight properties in object

--- jsnacks-5/assets/script.js
+++ jsnacks-5/assets/script.js
@@ -21,3 +21,7 @@ const biciclette = [
     nome: "Endurance",
   },
 ];
+
+const getRandomWeight = () => parseFloat((Math.random() * (5 - .5) +.5).toFixed(2));
+
+biciclette.map(bicicletta => bicicletta.peso = getRandomWeight());
