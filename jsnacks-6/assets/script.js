7e14a70d48656a841b4b856a85170152d7562d86
pierdomenicoguarnieri <pierdo.guarnieri@gmail.com>
2023-04-05 19:44:35 +0200

    add: card output

--- jsnacks-6/assets/script.js
+++ jsnacks-6/assets/script.js
@@ -41,6 +41,9 @@ const squadre = [
   },
 ];
 
+const container1 = document.getElementById("leaderboard");
+
+const container2 = document.getElementById("onyFouls");
 
 const randomPoints = () => Math.floor(Math.random()*85)+1;
 
@@ -58,3 +61,29 @@ const newSquadre = squadre.map(squadra => {
   return object;
 })
 
+squadre.forEach(squadra => {
+  const{nome, puntiFatti, falliSubiti} = squadra;
+  
+  container1.innerHTML +=`
+  <div class="card shadow-sm" style="width: 18rem;">
+    <div class="card-body">
+      <h5 class="card-title">${nome}</h5>
+      <p class="card-text">Punti fatti: ${puntiFatti}</p>
+      <p class="card-text">Falli subiti: ${falliSubiti}</p>
+    </div>
+  </div>
+`
+})
+
+squadre.forEach(squadra => {
+  const{nome, falliSubiti} = squadra;
+  
+  container2.innerHTML +=`
+  <div class="card shadow-sm" style="width: 18rem;">
+    <div class="card-body">
+      <h5 class="card-title">${nome}</h5>
+      <p class="card-text">Falli subiti: ${falliSubiti}</p>
+    </div>
+  </div>
+`
+})
