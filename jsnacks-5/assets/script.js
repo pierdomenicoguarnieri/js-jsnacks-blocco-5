43dba72331abf87fb796a46f15d3c75742240fc3
pierdomenicoguarnieri <pierdo.guarnieri@gmail.com>
2023-04-05 19:21:09 +0200

    add: array of objects

---
+++ jsnacks-5/assets/script.js
@@ -0,0 +1,23 @@
+const biciclette = [
+  {
+    nome: "Ciclocross",
+  },
+  {
+    nome: "Ebike",
+  },
+  {
+    nome: "Gravel",
+  },
+  {
+    nome: "Scatto Fisso",
+  },
+  {
+    nome: "Cronometro",
+  },
+  {
+    nome: "Aeroad",
+  },
+  {
+    nome: "Endurance",
+  },
+];
